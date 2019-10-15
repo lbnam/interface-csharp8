04a125f376176e9fe4f0fed6707b16c94c829ab1
LE Binh Nam <ble1@amaris.com>
2019-10-15 16:27:11 +0700

    test interface c#8

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace interface_csharp8
+{
+    public interface ICallable
+    {
+        string GetPlatform();
+        void Call()
+        {
+            Console.WriteLine("Calling..");
+        }
+    }
+
+    interface IAndroidCallable: ICallable
+    {
+        void Call()
+        {
+            Console.WriteLine("Calling from AndroidCallable");
+        }
+    }
+
+    interface IiPhoneCallable: ICallable
+    {
+        void Call()
+        {
+            Console.WriteLine("Calling from iPhoneCallable");
+        }
+    }
+
+    public class Android : IAndroidCallable
+    {
+        public string GetPlatform()
+        {
+            return "Android";
+        }
+    }
+
+    public class Ios : IiPhoneCallable
+    {
+        public string GetPlatform()
+        {
+            return "IOS";
+        }
+
+        public void Call()
+        {
+           Console.WriteLine("Calling IOS");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICallable phone = new Android();
+            phone.Call();
+
+            (phone as IAndroidCallable).Call();
+            (phone as ICallable).Call();
+            
+            phone = new Ios();
+            phone.Call();
+
+            (phone as IiPhoneCallable).Call();
+            (phone as ICallable).Call();    
+        }
+    }
+}
